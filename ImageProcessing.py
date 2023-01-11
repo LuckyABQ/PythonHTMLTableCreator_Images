19c987d34557c4570f4317c91841e37929289d93
LuckyABQ <garbe.lukas@gmx.de>
2023-01-11 13:20:33 +0100

    modified .gitignore and added ImageProcessing as file

---
+++ ImageProcessing.py
@@ -0,0 +1,222 @@
+from PIL import Image
+import random
+from typing import List, Dict
+from bs4 import BeautifulSoup
+from pathlib import Path
+import os
+
+from enum import Enum
+
+
+class WritingType(Enum):
+    HANDWRITING = 1
+    SIGNATURE = 2
+
+
+handwriting_root_path = \
+    Path(r'data/handwriting_images/')
+
+signatures_root_path = \
+    Path(r'data/signatures/')
+
+# todo: absolute path necessary for html
+temp_images_path = \
+    Path(r'/home/lukas/PycharmProjects/HTMLGeneration/PythonHtmlTableCreator/data/temp_images/')
+
+
+
+
+
+class ImageProcessor:
+    def __init__(self, number_images: int = 200, probability_signatures: float = 0.5):
+        self.signature = None
+        self.handwriting = None
+        self.number_images = number_images
+        self.probability_signatures = probability_signatures
+        self.counter_signatures = 0
+        self.counter_handwriting = 0
+        self.preload_images(number_images)
+        self.clean_up()
+
+    def clean_up(self):
+        """
+        deletes all preloaded, and pre processed images
+        :return: none
+        """
+        removing_files = temp_images_path.glob('*.png')
+        for r in removing_files:
+            os.remove(r)
+
+    def preload_images(self, number_images: int) -> None:
+        """
+        determines used images for next file - containing signatures and normal handwriting
+        metadata stored in self.handwriting and self.signature
+        :param number_images: loads number of images for each type, handwriting and signatures
+        :return:
+        """
+        # for each preloaded image we need its absolute path (=source), its name,
+        # and its writing_type (signature or handwriting)
+
+        # preloading of handwriting images:
+        # the text inside the images is only loaded, when needed
+        all_handwriting_images = [x for x in handwriting_root_path.rglob('*.png')]
+        chosen_handwriting_files = random.choices(all_handwriting_images, k=number_images)
+        self.handwriting = [{'path': str(x), 'name': x.name, 'writing_type': str(WritingType.HANDWRITING)} for x in
+                            chosen_handwriting_files]
+
+        # load signatures
+        suffixes = ['.png', '.PNG']
+        all_signature_images = [x for x in signatures_root_path.rglob('*') if x.suffix in suffixes]
+        chosen_signature_files = random.choices(all_signature_images, k=number_images)
+        self.signature = [{'path': str(x), 'name': x.name, 'writing_type': str(WritingType.SIGNATURE)} for x in
+                          chosen_signature_files]
+
+    def get_next_image(self) -> (str, str, str, str, str):
+        """
+        Preprocesses image for html usage
+        :return: Dictionary containing path, name, writing_type. transform and if writing_type == handwriting, text
+        """
+        next_image = None
+        if random.random() < self.probability_signatures:
+            next_image = self.signature[self.counter_signatures]
+            self.counter_signatures += 1
+
+        else:
+
+            # some images contain just one element - which is considered not long enough
+            is_long_enough = False
+
+            while not is_long_enough:
+                # get next image from handwriting list
+                next_image = self.handwriting[self.counter_handwriting]
+                self.counter_handwriting += 1
+
+                # determine name of xml file (for a01-000u-00-01 it is a01-000u, therefore split and join)
+                xml_name = '-'.join(next_image['name'].split('-')[0:2])
+
+                # open xml file and extract data
+                with open(str(next(handwriting_root_path.rglob(xml_name + ".xml"))), 'r') as f:
+                    data = f.read()
+
+                # find the word in xml file, and its text
+                soup = BeautifulSoup(data, "xml")
+                meta_data_word = soup.find(id=next_image['name'][:-4])
+                next_image['text'] = meta_data_word['text']
+                is_long_enough = len(next_image['text']) > 1
+
+        next_image['transform'] = get_rand_transform()
+
+        # make_white_area_transplant returns path for temporary file, with transparent background
+        next_image['path'] = store_images_for_masks(next_image)
+
+        return next_image
+
+
+# todo: overlapping with table boarders
+def get_rand_transform(img_size: List[int] = [80, 25], box_size: List[float] = [160, 50] ) -> str:
+    transform = f"transform: translateX({get_rand_translate(img_size[0], box_size[0])}px) " \
+                f"translateY({get_rand_translate(img_size[1], box_size[1])}px)" \
+                f"rotate({get_rand_rotation()}deg)"
+    return transform
+
+
+def get_rand_translate(img_size: float, box_size: float) -> str:
+    # Translation: max one quarter of the image should overlap the table border
+    max_translate = (box_size - img_size) * 0.5 + img_size * 0.25
+    translate = random.uniform(-max_translate, max_translate)
+    return translate
+
+
+def get_rand_rotation() -> float:
+    # rotation of max +-30deg
+    rotation = random.uniform(-25, 25)
+    return rotation
+
+
+def get_rand_scale() -> str:
+    # Scaling of 0,9-1,1
+    scale = 1 + (random.random() - 0.5) * 0.1
+    return f"scale({scale}deg)"
+
+
+def calc_transparency(background_grey: int, darkest_grey: int, grey_value: int) -> int:
+    """
+    Calculates transparency value for a given grey_value
+    """
+
+
+    linear_transparency = 255 - grey_value
+    quadratic_transparency = int(((255 - grey_value) / 255) ** 2 * 255)
+    cubic_transparency = int(((255 - grey_value) / 255) ** 3 * 255)
+    quartic_transparency = int(((255 - grey_value) / 255) ** 4 * 255)
+
+    # grey_value is 0 if pixel is black
+    # alpha_chanel is 0 if fully transparent
+
+    # pixel is almost white => set to transparent
+    if grey_value > background_grey - 40:
+        non_differentiable = 0
+
+    else:
+        non_differentiable = 255
+
+   # grey_value < darkest_grey - 30:
+
+    return non_differentiable
+
+
+
+
+def store_images_for_masks(next_image: Dict) -> str:
+    """
+     preprocessing of image: saves binary and semitransparent images in temp_images_path
+     semi-transparent image: everything transparent except handwriting/signature
+    :return: returns new path of semi-transparent image
+    """
+    im = Image.open(next_image['path'])
+    im = im.convert("RGBA")
+    data_rgba = im.getdata()
+
+    # save not_transparent_image -> debugging
+    # output_path = temp_images_path / ("not_transparent_" + next_image['name'])
+    # im.save(output_path, "PNG")
+
+    # grey_scale is used to determine how transparent a pixel should be
+    image_grey = im.convert("L")
+    data_grey = image_grey.getdata()
+
+    transparent_data = []
+    binary_data = []
+
+    background_grey = max(data_grey)
+    darkest_grey = min(data_grey)
+    for item in zip(data_rgba, data_grey):
+        transparency = calc_transparency(background_grey, darkest_grey, item[1])
+        transparent_data.append((item[0][0], item[0][1], item[0][2], transparency))
+        if transparency == 0:
+            binary_data.append((0, 0, 0, 0))
+        else:
+            binary_data.append((0, 0, 0, 255))
+
+    im.putdata(transparent_data)
+
+    # save image without background
+    output_path = temp_images_path / ("no_background_" + next_image['name'])
+    im.save(output_path, "PNG")
+
+    # save image as binary
+    im.putdata(binary_data)
+    output_path_binary = temp_images_path / ("binary_" + next_image['name'])
+    im.save(output_path_binary, "PNG")
+
+    return output_path
+
+
+# Debugging: Transparency
+suffixes = ['.png', '.PNG']
+all_signature_images = [x for x in signatures_root_path.rglob('*') if x.suffix in suffixes]
+chosen_signature_files = random.choices(all_signature_images, k=200)
+signatures = [{'path': str(x), 'name': x.name, 'writing_type': str(WritingType.SIGNATURE)} for x in
+                  chosen_signature_files]
+signature = signatures[0]
+store_images_for_masks(signature)
